dc9f7dbcf16bf511c15d57184b62bdf5b6ee6677
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2017-06-15 10:14:08 -0500

    Determined exactly what functions are needed

--- converter.js
+++ converter.js
@@ -16,3 +16,18 @@ var resulth3 = document.getElementById("resultsAppear");
 var userInput;
 
 var result;
+
+
+// Event listeners 
+
+
+
+
+// Functions Needed
+
+// 1. Function to determine which conversion should happen based on which radio button is selected
+// 2. Function to convert userInput from Fahrenheit to Celcius
+// 3. Function to convert userInput from Celcius to Fahrrenheit
+// 4. Function to change color of result output to h3 element depending on value of result (if/else if/else) 
+// 5. Function that, when user clicks "Enter" in input field, fires the conversion function
+// 6. Function to clear input field when user clicks "Clear" button
