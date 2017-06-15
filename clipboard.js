98e293c22b1c2e5bc4d3b3668f09e59045a0fa73
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2017-06-15 11:25:23 -0500

    Added event listener to convert button and wrote function to determine which radio button is selected

---
+++ clipboard.js
@@ -0,0 +1,16 @@
+whichScale = function () {
+
+  if (fahrRadio.checked) {
+
+      console.log("Fahrenheit is selected.");
+
+      return true;
+  }
+
+  else {
+
+    console.log("Celsius is selected");
+
+    return false;
+  }
+};
--- converter.js
+++ converter.js
@@ -1,5 +1,6 @@
-
-// Variables for referencing HTML elements
+//////////////////////////////////////////////
+// Variables for referencing HTML elements //
+////////////////////////////////////////////
 
 var inputBox = document.getElementById("tempInput");
 
@@ -18,16 +19,40 @@ var userInput;
 var result;
 
 
-// Event listeners 
-
-
-
-
-// Functions Needed
+///////////////////////
+// Functions Needed //
+/////////////////////
 
 // 1. Function to determine which conversion should happen based on which radio button is selected
+
+var whichScale = function () {
+
+  if (fahrRadio.checked) {
+
+      console.log("Fahrenheit is selected.");
+
+      return true;
+  }
+
+  else {
+
+    console.log("Celsius is selected");
+
+    return false;
+  }
+};
+
+
 // 2. Function to convert userInput from Fahrenheit to Celcius
 // 3. Function to convert userInput from Celcius to Fahrrenheit
 // 4. Function to change color of result output to h3 element depending on value of result (if/else if/else) 
 // 5. Function that, when user clicks "Enter" in input field, fires the conversion function
 // 6. Function to clear input field when user clicks "Clear" button
+
+
+//////////////////////
+// Event listeners // 
+////////////////////
+
+// Add an event listener to the "convert" button; when clicked, the whichScale function is called
+convertBtn.addEventListener("click", whichScale);
