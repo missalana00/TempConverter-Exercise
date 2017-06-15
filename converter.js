14a77f24f988a9899eb5f2d566ed93cf22b8b048
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2017-06-15 09:48:39 -0500

    Created variables for referencing HTML elements

---
+++ converter.js
@@ -0,0 +1,18 @@
+
+// Variables for referencing HTML elements
+
+var inputBox = document.getElementById("tempInput");
+
+var fahrRadio = document.getElementById("fahrenheitRadio");
+
+var celsRadio = document.getElementById("celsiusRadio");
+
+var convertBtn = document.getElementById("convertButton");
+
+var clearBtn = document.getElementById("clearButton");
+
+var resulth3 = document.getElementById("resultsAppear");
+
+var userInput;
+
+var result;
